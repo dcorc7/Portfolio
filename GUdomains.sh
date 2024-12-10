19353e46850be307521150b1bf8102e412b76adb
David Corcoran <dc1524@georgetown.edu>
2024-12-10 10:04:38 -0500

    Updated home page and added 500 project link

--- GUdomains.sh
+++ GUdomains.sh
@@ -1,7 +1,7 @@
 
 
 echo "Cleaning up existing build..."
-rm -rf Portfolio-Website
+rm -rf Homepage
 
 # build website
 echo "Building the website..."
@@ -10,16 +10,16 @@ quarto render
 # Set Correct File Permissions
 echo "Setting correct file permissions..."
 
-find Portfolio-Website -type f -exec chmod 644 {} \;
+find Homepage -type f -exec chmod 644 {} \;
 
-find Portfolio-Website -type d -exec chmod 755 {} \;
+find Homepage -type d -exec chmod 755 {} \;
 
 # push to the Website
 read -p "Do you want to push the website to your GU domains folder? (y/n): " answer
 
 if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
     echo "Pushing the website to the remote server..."
-    scp -r Portfolio-Website corcoran@corcoran.georgetown.domains:/home/corcoran/public_html/
+    scp -r Homepage corcoran@corcoran.georgetown.domains:/home/corcoran/public_html/
     echo "Website successfully pushed!"
 else
     echo "Skipping deployment."
